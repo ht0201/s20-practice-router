7f0c81c56f658109be2e84a961d8deee663fbdc1
thai.vo <43738511+ht0201@users.noreply.github.com>
2022-08-15 14:59:57 +0700

    getAll and post cmt

--- src/lib/api.js
+++ src/lib/api.js
@@ -49,3 +49,46 @@ export const getSingleQuote = async (quoteId) => {
 
   return loadedQuote;
 };
+
+export const addComment = async (requestData) => {
+  const res = await fetch(
+    `${FIREBASE_DOMAIN}/comments/${requestData.quoteId}.json`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(requestData.commentData),
+    }
+  );
+
+  const data = await res.json();
+  if (!res.ok) {
+    throw new Error(data.message || 'Could not sent comment !');
+  }
+
+  return null;
+};
+
+export async function getAllComments(quoteId) {
+  const response = await fetch(`${FIREBASE_DOMAIN}/comments/${quoteId}.json`);
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || 'Could not get comments.');
+  }
+
+  const transformedComments = [];
+
+  for (const key in data) {
+    const commentObj = {
+      id: key,
+      ...data[key],
+    };
+
+    transformedComments.push(commentObj);
+  }
+
+  return transformedComments;
+}
